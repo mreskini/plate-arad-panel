0d3ae75b0f9184c5ce7a0c79da5c21a19d80db14
sepehr-eskini <sepehr.eskini@gmail.com>
2025-10-08 19:50:22 +0330

    Feat: Add devices and related modals

--- src/core/api/gql/generated/operations.ts
+++ src/core/api/gql/generated/operations.ts
@@ -1,5 +1,29 @@
 import * as Types from './schemas';
 
+export type FetchDevicesQueryVariables = Types.Exact<{ [key: string]: never; }>;
+
+
+export type FetchDevicesQuery = { fetchDevices: Array<{ token: string, type: Types.E_DeviceType, ip: string, name: string, brand_name?: string | null, username?: string | null, password?: string | null }> };
+
+export type CreateDeviceMutationVariables = Types.Exact<{
+  body: Types.CreateDeviceRq;
+}>;
+
+
+export type CreateDeviceMutation = { createDevice: boolean };
+
+export type EditDeviceMutationVariables = Types.Exact<{
+  body: Types.EditDeviceRq;
+}>;
+
+
+export type EditDeviceMutation = { editDevice: boolean };
+
+export type PingAllDevicesQueryVariables = Types.Exact<{ [key: string]: never; }>;
+
+
+export type PingAllDevicesQuery = { pingAllDevices: Array<{ name: string, ip: string, type: Types.E_DeviceType, is_alive: boolean }> };
+
 export type ParkingInfoQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
 
