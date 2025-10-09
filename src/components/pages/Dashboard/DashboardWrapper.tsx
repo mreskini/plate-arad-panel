c7772705fbedb13a2a2a494c2aca658ef03de842
sepehr-eskini <sepehr.eskini@gmail.com>
2025-10-09 10:59:37 +0330

    Feat: Tile cameras in dashboard and remove camera connection form

---
+++ src/components/pages/Dashboard/DashboardWrapper.tsx
@@ -0,0 +1,23 @@
+import { Text } from "@components/template"
+import clsx from "clsx"
+import type { KeysWithoutReturnObjects } from "i18next"
+import type { FC, ReactNode } from "react"
+
+interface I_Props {
+    title?: KeysWithoutReturnObjects["common"]
+    children: ReactNode
+    className?: string
+}
+
+export const DashboardWrapper: FC<I_Props> = ({ title, children, className }) => {
+    return (
+        <div className={clsx(["rounded-2xl h-full border border-neutral-200 p-5", className])}>
+            {title && (
+                <div className="mb-10">
+                    <Text className="text-blue-500" weight={700} variant="heading-6" contentKey={title} ns="common" />
+                </div>
+            )}
+            <div className="w-full h-full">{children}</div>
+        </div>
+    )
+}
